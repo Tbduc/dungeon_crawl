c501b3810fe9a37fcf1265c7723ac03b7cb8712b
Duc Trung Bui <txbuiduc@gmail.com>
2022-12-05 07:18:14 +0100

    Add game to git

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
@@ -0,0 +1,45 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.Drawable;
+
+import java.util.Objects;
+
+public abstract class Item implements Drawable {
+
+    private Cell cell;
+
+    protected Item(Cell cell) {
+        this.cell = cell;
+        this.cell.setItem(this);
+    }
+
+    public Cell getCell() {
+        return cell;
+    }
+
+    public void setCell(Cell cell) {
+        this.cell = cell;
+    }
+
+    public int getX() {
+        return cell.getX();
+    }
+
+    public int getY() {
+        return cell.getY();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(cell, item.cell);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cell);
+    }
+}
